6d45dfe9a0a80c7943d95bc2bdd9722f9a0ddeb9
Junhao "Jerry" Zhang <house_comix@sina.com>
2017-12-11 03:27:09 +0800

    Create problems.js

---
+++ problems.js
@@ -0,0 +1,57 @@
+//Add problems or fix typos, please do not remove any questions or this comment. Thank you (:
+/*Reminder:The answer checker can check only numbers.*/
+var problems = [
+    {
+        sayings:"1+1",
+        answer:1+1,
+    },
+    {
+        sayings:"234+99",
+        answer:234+99,
+    },
+    {
+        sayings:"1234*20",
+        answer:1234*20,
+    },
+    {
+        sayings:"100÷5",
+        answer:100/5,
+    },
+    {
+        sayings:"John had 500 cookies. Bob has 200. John decides to give Bob 20 cookies. Bob decides to give John 30 cookies. How many cookies does John have now?",
+        answer:(500-20)+30,
+    },
+    {
+        sayings:"How many dots are there? <br>&#11044;&#11044;&#11044;&#11044;",
+        answer:4,
+    },
+    {
+        sayings:"What is |-5|?",
+        answer:5,
+    },
+    {
+        sayings:"What is &#8730;9?",
+        answer:3,
+    },
+    {
+        sayings:"9&times;20",
+        answer:9*20,
+    },
+    {
+        sayings:"9&times;150",
+        answer:9*150,
+    },
+    {
+        sayings:"John is 18. His brother is half of his age. When John is 20, how old is his brother?",
+        answer:9+2,
+    },
+    {
+        sayings:"What is &#8730;100?",
+        answer:10,
+    },
+    {
+        sayings:"(|-500|+20)÷10",
+        answer:(500+20)÷10,
+    },
+    
+];
